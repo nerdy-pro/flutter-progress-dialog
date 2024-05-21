4723fafc72601a289f137993c5269a7303d9b6b6
Ilya Nixan <thenixan@gmail.com>
2024-05-21 02:59:24 +0200

    Flutter update

--- lib/dialog/progress_bar_dialog.dart
+++ lib/dialog/progress_bar_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_future_progress_dialog/result.dart';
+import 'package:flutter_future_progress_dialog/dialog/result.dart';
 
 /// Widget displays the [Dialog] white the provided [future] is being evaluated.
 /// Optionally you can provide a custom [builder] which will be used instead of the default [Dialog].
@@ -7,11 +7,7 @@ class ProgressBarDialog<T> extends StatefulWidget {
   final Future<T> Function() future;
   final WidgetBuilder? builder;
 
-  const ProgressBarDialog({
-    super.key,
-    required this.future,
-    this.builder,
-  });
+  const ProgressBarDialog({super.key, required this.future, this.builder});
 
   @override
   State<StatefulWidget> createState() => _ProgressBarDialogState<T>();
--- lib/flutter_future_progress_dialog.dart
+++ lib/flutter_future_progress_dialog.dart
@@ -1,4 +1,4 @@
 library flutter_future_progress_dialog;
 
-export 'result.dart';
-export 'show_progress_dialog.dart';
+export 'dialog/result.dart';
+export 'dialog/show_progress_dialog.dart';
